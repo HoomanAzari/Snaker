b131184f9eda2d9b064a553c598a5fa37c948560
Hooman Azari <hooazari@gmail.com>
2023-03-06 21:09:14 -0500

    Added MyLinkedList.java

---
+++ src/assignment2/MyLinkedList.java
@@ -0,0 +1,14 @@
+package assignment2;
+
+public class MyLinkedList<E> implements MyList<E> {
+    public int getSize() {
+        return MyLinkedList<E>.length
+    }
+
+    public boolean isEmpty() {
+
+    }
+    public boolean add();
+    public void clear();
+    public E remove();
+}
--- src/assignment2/MyList.java
+++ src/assignment2/MyList.java
@@ -2,6 +2,11 @@ package assignment2;
 
 public interface MyList<E> extends Iterable<E> {            //E stands for element
     public int getSize();
+    public boolean isEmpty();
+    public boolean add();
+    public void clear();
+    public E remove();                      // returns an element of type E
 
-    public
-}
+    }
+
+
